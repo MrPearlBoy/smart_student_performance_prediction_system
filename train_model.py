0e487fac5aa9472bb342eddaca1f8794430e8402
Muthuselvan S <muthuselvan.gceb@gmail.com>
2026-08-19 11:01:29 +0000

    Changes to be committed: new file:   Students Performance Dataset.csv new file:   student_regression_model.pkl modified:   train_model.py

--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import joblib
 
@@ -9,20 +10,25 @@ data = pd.read_csv("Students Performance Dataset.csv")
 print("Dataset loaded successfully!")
 print("Number of students:", len(data))
 
-X = data[[ "Attendance,Internal_marks,Assignments_Avg,Previous_preformance,Study_Hours_per_Day"]]
+X = data[[ "Attendance", "Internal_marks", "Assignments_Avg", "Previous_preformance", "Study_Hours_per_Day"]]
 
 # Select target
 y = data["Performance"]
 
 # Split dataset
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=42,stratify=y)
 
 print("Training data:", len(X_train))
 print("Testing data:", len(X_test))
 
-# Create ML model
-model = RandomForestClassifier(n_estimators=100,random_state=42)
+scaler = StandardScaler()
 
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# Create ML model
+model = LogisticRegression( max_iter=50000, random_state=42 )
+    
 # Train model
 model.fit(X_train, y_train)
 
@@ -32,9 +38,9 @@ y_pred = model.predict(X_test)
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
 
-print("Model Accuracy:", accuracy)
+print("Regression Model Accuracy:", accuracy)
 
 # Save trained model
-joblib.dump(model, "student_model.pkl")
+joblib.dump(model, "student_regression_model.pkl")
 
 print("Model saved successfully!")
